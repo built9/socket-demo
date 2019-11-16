4eefe7fe74561118f3229129494ec5bb9620aece
cCedar <771524005@qq.com>
2019-11-16 12:56:35 +0800

    普通登录

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const querystring = require("querystring");
 const jwt = require("jsonwebtoken");
 const jwtAuth = require("koa-jwt");
 
-const server = require('http').Server(app.callback());
-const io = require('socket.io')(server);
+const server = require("http").Server(app.callback());
+const io = require("socket.io")(server);
 
-const accessTokens = {}
+const accessTokens = {};
 const secret = "secret";
 const config = {
   client_id: "33ed6e40cdb09c60448b",
@@ -18,7 +18,6 @@ const config = {
 };
 
 app.use(static(__dirname + "/"));
-
 
 router.get("/auth/github/login", async ctx => {
   var dataStr = new Date().valueOf();
@@ -28,53 +27,63 @@ router.get("/auth/github/login", async ctx => {
   ctx.redirect(path);
 });
 router.get("/github/oauth/callback", async ctx => {
-	const code = ctx.query.code
-	const params = {
-        client_id: config.client_id,
-        client_secret: config.client_secret,
-        code: code
-	}
-	let res = await axios.post('https://github.com/login/oauth/access_token', params)
-    const access_token = querystring.parse(res.data).access_token
-    const uid = Math.random() * 99999
-    accessTokens[uid] = access_token
+  const code = ctx.query.code;
+  const params = {
+    client_id: config.client_id,
+    client_secret: config.client_secret,
+    code: code
+  };
+  let res = await axios.post(
+    "https://github.com/login/oauth/access_token",
+    params
+  );
+  const access_token = querystring.parse(res.data).access_token;
+  const uid = Math.random() * 99999;
+  accessTokens[uid] = access_token;
 
-    const token = jwt.sign(
-        {
-            data: uid,
-            // 设置 token 过期时间，一小时后，秒为单位
-            exp: Math.floor(Date.now() / 1000) + 60 * 60
-        },
-        secret
-    )
-    ctx.response.type = 'html';
-    ctx.response.body = ` <script>window.localStorage.setItem("authSuccess","true");window.localStorage.setItem("token","${token}");window.close();</script>`;
+  const token = jwt.sign(
+    {
+      data: uid,
+      // 设置 token 过期时间，一小时后，秒为单位
+      exp: Math.floor(Date.now() / 1000) + 60 * 60
+    },
+    secret
+  );
+  ctx.response.type = "html";
+  ctx.response.body = ` <script>window.localStorage.setItem("authSuccess","true");window.localStorage.setItem("token","${token}");window.close();</script>`;
 });
 
-router.get('/auth/github/userinfo', jwtAuth({
-	secret
-}), async (ctx) => {
-	// 验证通过，state.user
-	console.log('jwt playload:', ctx.state.user)
-	const access_token = accessTokens[ctx.state.user.data]
-	res = await axios.get('https://api.github.com/user?access_token=' + access_token)
-	console.log('userAccess:', res.data)
-	ctx.body = res.data
-})
-
+router.get(
+  "/auth/github/userinfo",
+  jwtAuth({
+    secret
+  }),
+  async ctx => {
+    // 验证通过，state.user
+    console.log("jwt playload:", ctx.state.user);
+    const access_token = accessTokens[ctx.state.user.data];
+    res = await axios.get(
+      "https://api.github.com/user?access_token=" + access_token
+    );
+    console.log("userAccess:", res.data);
+    ctx.body = res.data;
+  }
+);
+router.get("/login", async ctx => {
+  ctx.body = ctx.query;
+});
 app.use(router.routes()); /*启动路由*/
 app.use(router.allowedMethods());
 
-io.on('connection', (socket) => {
-	console.log(1111111);
-	socket.on('chat message', (msg) => {
-	  console.log('message: '+msg);
-	  io.emit('chat message', msg);
-	});
-	socket.on('disconnect', () => {
-	  console.log('user disconnected');
-	});
+io.on("connection", socket => {
+  socket.on("chat message", msg => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
   });
-  server.listen(3000, () => {
-	console.log('app run at : http://127.0.0.1:3000');
-})
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+server.listen(3000, () => {
+  console.log("app run at : http://127.0.0.1:3000");
+});
